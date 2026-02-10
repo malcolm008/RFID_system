0f9a409d2fbc65930f665d87a4bef8eb19c7cc7d
malcolm008 <mac.mwaki@mecs.co.tz>
2026-02-10 10:22:28 +0300

    urls.py 2 update

--- backend/attendance_api/urls.py
+++ backend/attendance_api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import StudentListView, CreateStudentView, UpdateStudentView
+from .views import StudentListView, CreateStudentView, UpdateStudentView, TeacherListView, CreateTeacherView, UpdateTeacherView
 
 urlpatterns = [
     path('list/', StudentListView.as_view(), name='student_list'),
     path('create/', CreateStudentView.as_view(), name='create_student'),
     path('update/', UpdateStudentView.as_view(), name='update_student'),
+    path('list/', TeacherListView.as_view(), name='teacher_list'),
+    path('create/', CreateTeacherView.as_view(), name='create_teacher'),
+    path('update/', UpdateTeacherView.as_view(), name='update_teacher'),
 ]
