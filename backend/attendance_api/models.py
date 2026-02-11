d4acfa7c471e5225b0bb85c25bfd27638157cad6
malcolm008 <mac.mwaki@mecs.co.tz>
2026-02-11 20:39:38 +0300

    models.py update

--- backend/attendance_api/models.py
+++ backend/attendance_api/models.py
@@ -85,3 +85,19 @@ class Program(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.qualification})"
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=20)
+
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    department = models.CharField(max_length=100)
+
+    semester = models.IntegerField()
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
+
