e94631a6aabf35586defd0f3dcdc05c5f6c9cf7d
malcolm008 <mac.mwaki@mecs.co.tz>
2026-02-11 20:32:45 +0300

    course model update

--- frontend_ui/lib/features/programs/course_model.dart
+++ frontend_ui/lib/features/programs/course_model.dart
@@ -2,42 +2,47 @@ class Course {
   final String id;
   final String name;
   final String code;
-  final String department;
+
+  final String programId;
+  final String departmentId;
+  final String teacherId;
+
   final int semester;
   final int year;
-  final String teacherName;
 
   Course({
     required this.id,
     required this.name,
     required this.code,
-    required this.department,
+    required this.programId,
+    required this.departmentId,
+    required this.teacherId,
     required this.semester,
     required this.year,
-    required this.teacherName,
-  });
+});
 
   factory Course.fromJson(Map<String, dynamic> json) {
     return Course(
       id: json['id'].toString(),
       name: json['name'],
       code: json['code'],
-      department: json['department'],
+      programId: json['program'].toString(),
+      departmentId: json['department'].toString(),
+      teacherId: json['teacher'].toString(),
       semester: int.parse(json['semester'].toString()),
       year: int.parse(json['year'].toString()),
-      teacherName: json['teacherName'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      "id": id,
       "name": name,
       "code": code,
-      "department": department,
+      "program": programId,
+      "department": departmentId,
+      "teacher": teacherId,
       "semester": semester,
       "year": year,
-      "teacherName": teacherName,
     };
   }
 }
