c359395f0e35309bbd1ceba3c7920efed4082c3c
malcolm008 <mac.mwaki@mecs.co.tz>
2026-02-17 22:26:49 +0300

    Url update

--- backend/attendance_api/urls.py
+++ backend/attendance_api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import (
-    StudentListView, CreateStudentView, UpdateStudentView,
-    TeacherListView, CreateTeacherView, UpdateTeacherView,
-    DeviceListView, CreateDeviceView, UpdateDeviceView,
-    CreateProgramView, UpdateProgramView, ProgramListView,
-    CourseListView, CreateCourseView, UpdateCourseView
+    StudentListView, CreateStudentView, UpdateStudentView, DeleteStudentView, BulkDeleteStudentView,
+    TeacherListView, CreateTeacherView, UpdateTeacherView, DeleteTeacherView, BulkDeleteTeacherView,
+    DeviceListView, CreateDeviceView, UpdateDeviceView, DeleteDeviceView, BulkDeleteDeviceView,
+    CreateProgramView, UpdateProgramView, ProgramListView, DeleteProgramView, BulkDeleteProgramView,
+    CourseListView, CreateCourseView, UpdateCourseView, DeleteCourseView, BulkDeleteCourseView
 )
 
 urlpatterns = [
@@ -12,24 +12,38 @@ urlpatterns = [
     path('students/list/', StudentListView.as_view(), name='student_list'),
     path('students/create/', CreateStudentView.as_view(), name='create_student'),
     path('students/update/', UpdateStudentView.as_view(), name='update_student'),
+    path('students/delete/', DeleteStudentView.as_view(), name='delete_student'),
+    path('students/bulk-delete/', BulkDeleteStudentView.as_view(), name='bulk-delete_student'),
 
     # TEACHERS
     path('teachers/list/', TeacherListView.as_view(), name='teacher_list'),
     path('teachers/create/', CreateTeacherView.as_view(), name='create_teacher'),
     path('teachers/update/', UpdateTeacherView.as_view(), name='update_teacher'),
+    path('teachers/delete/', DeleteTeacherView.as_view(), name='delete_teacher'),
+    path('teachers/bulk-delete/', BulkDeleteTeacherView.as_view(), name='bulk_delete_teacher'),
+
 
     #DEVICE
     path('devices/list/', DeviceListView.as_view()),
     path('devices/create', CreateDeviceView.as_view()),
     path('devices/update', UpdateDeviceView.as_view()),
+    path('devices/delete/', DeleteDeviceView.as_view()),
+    path('devices/bulk-delete/', BulkDeleteDeviceView.as_view()),
+
 
     #PROGRAM
     path('programs/list/', ProgramListView.as_view()),
     path('programs/create/', CreateProgramView.as_view()),
     path('programs/update/', UpdateProgramView.as_view()),
+    path('programs/delete/', DeleteProgramView.as_view()),
+    path('programs/bulk-delete/', BulkDeleteProgramView.as_view()),
+
 
     #COURSE
     path('courses/list/', CourseListView.as_view(), name='course_list'),
     path('courses/create/', CreateCourseView.as_view(), name='create_course'),
     path('courses/update/', UpdateCourseView.as_view(), name='update_course'),
+    path('courses/delete/', DeleteCourseView.as_view(), name='delete_course'),
+    path('courses/bulk-delete/', BulkDeleteCourseView.as_view(), name='bulk_delete_course'),
+
 ]
